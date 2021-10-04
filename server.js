0348742a1c87b9bf7e42cb77e48694c9749d8c14
Nina Kim <neenerskim@gmail.com>
2021-10-04 14:28:13 -0700

    add buy button functionality and come edit view

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
+const methodOverride = require('method-override')
 const Product = require('./models/products')
 const productSeed = require('./models/productSeed')
 const User = require('./models/user')
@@ -21,6 +22,7 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(logger('dev'))
+app.use(methodOverride('_method'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -46,7 +48,7 @@ app.get('/products/new', (req, res) => {
     res.render('new.ejs')
 })
 
-app.post('/products', (req, res) => {
+app.post('/products', (req, res) => {   
     const newProduct = {
         name: req.body.name,
         description: req.body.description,
@@ -64,10 +66,57 @@ app.get('/products/:id', (req, res) => {
     Product.findById(req.params.id, (err, foundProduct) => {
         res.render('show.ejs', {
             product: foundProduct,
+            index: req.params.id,
         })
     })
 })
 
+
+
+// Update route
+// updates buy button
+
+
+app.get('/products/:id/edit', (req, res) => {
+    Product.findById(req.params.id, (err, editProduct) => {
+        res.render('edit.ejs', {
+            product: editProduct,
+            index: req.params.id,
+        })
+    })
+
+})
+
+// updates product
+app.put('/products/:id/edit', (req, res) => {
+    Product.findById(req.params.id, (err, product) => {
+        product.name = req.body.name
+        product.description = req.body.description
+        product.img = req.body.img
+        product.price = Number(req.body.price)
+        product.qty = Number(req.body.qty)
+
+
+        product.save(err => {
+            if (err) return res.render(`products/${req.params.id}/edit`, {
+                product, index: req.params.id
+            })
+
+            res.redirect(`/products/${product._id}`)
+        })
+    })
+})
+
+app.put('/products/:id', (req, res) => {
+    Product.findById(req.params.id, (err, product) => {
+        product.qty -= 1
+        console.log(product.qty)
+        product.save(err => {
+            res.redirect(`/products/${product._id}`)
+        })
+    })
+
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
